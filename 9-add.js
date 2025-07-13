3a0a640efd0d7b03d55f09ca16fd898857f1e33a
BangerBoy <bookingsday@gmail.com>
2025-07-13 15:17:26 +0000

    Add file 9-add.js that prints addition of 2 integers

---
+++ 9-add.js
@@ -0,0 +1,7 @@
+function add(a, b){
+    return a + b;
+}
+const args = process.argv.slice(2);
+const firstInt = parseInt(args[0]);
+const secondInt = parseInt(args[1]);
+console.log(add(firstInt, secondInt));
