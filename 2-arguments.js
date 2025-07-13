20bc8a6b5002013bfea86b6f7e83f3447d41345b
BangerBoy <bookingsday@gmail.com>
2025-07-13 13:14:13 +0000

    Added 2-arguments.js with argument message depending on the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2);
+const argCount = args.length;
+if (argCount === 0){
+    console.log("No Argument");
+} else if (argCount === 1){
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
